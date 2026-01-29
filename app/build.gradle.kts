024154b818ed004a29d0bf7e50bfa52a95e61a9f
Aurelio-0-0 <victoperez@csumb.edu>
2026-01-29 12:38:14 -0800

    Add Gson converter dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,10 @@ dependencies {
     //hopefully it fixes
     implementation("androidx.appcompat:appcompat:1.7.1")
 
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+
     // Coroutines (for suspend/network calls)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation(libs.androidx.ui)
