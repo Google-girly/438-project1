31b1f7b1cfb3e0da5b54fed32650409bbe1322e6
yadhiramorales <yadhimorales45@gmail.com>
2026-01-28 20:11:03 -0800

    fix game and login mappings for setContentView

--- app/src/main/java/com/example/a438_project1/GameActivity.kt
+++ app/src/main/java/com/example/a438_project1/GameActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_game)
     }
 }
